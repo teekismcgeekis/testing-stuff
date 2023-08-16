af4392f9300c51d1ca7a5200a07ed0321d354f00
TK <31575140+teekismcgeekis@users.noreply.github.com>
2023-08-16 09:58:59 -0700

    Update Powershell.ps1

--- Powershell.ps1
+++ Powershell.ps1
@@ -2,5 +2,4 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force
 
 Start-Process "https://google.com"
 
-Invoke-WebRequest https://app.ninjarmm.com/agent/installer/91ee3a42-ac76-4dfc-ab98-f00ed3f8e4b1/mytsupportmainoffice-5.3.7022-windows-installer.msi -OutFile C:\ProgramData\test.msi"
 
