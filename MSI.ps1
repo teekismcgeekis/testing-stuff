f904f07fa890755f86c57cc083cc914090cad588
TK <31575140+teekismcgeekis@users.noreply.github.com>
2023-08-16 10:21:28 -0700

    Update MSI.ps1

--- MSI.ps1
+++ MSI.ps1
@@ -1,5 +1,3 @@
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force
-
 #Download and Run MSI package for Automated install
 $url = "https://app.ninjarmm.com/agent/installer/91ee3a42-ac76-4dfc-ab98-f00ed3f8e4b1/mytsupportmainoffice-5.3.7022-windows-installer.msi"
 $dest = "c:\ProgramData\installer.msi"
